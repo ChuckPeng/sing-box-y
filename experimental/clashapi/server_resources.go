6198489af6c4e7ee6cff57582e424a978a69993c
yelnoo <t50500411@163.com>
2025-04-14 10:37:20 +0800

    clash-api: Add update external-ui support

--- experimental/clashapi/server_resources.go
+++ experimental/clashapi/server_resources.go
@@ -77,6 +77,7 @@ func (s *Server) downloadExternalUI() error {
 	if response.StatusCode != http.StatusOK {
 		return E.New("download external ui failed: ", response.Status)
 	}
+	removeAllInDirectory(s.externalUI)
 	err = s.downloadZIP(response.Body, s.externalUI)
 	if err != nil {
 		removeAllInDirectory(s.externalUI)
